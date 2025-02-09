66c8796dea82a7bf8826ee8ff58b0bf4a2f93b1d
swan28 <swati28.g@gmail.com>
2025-02-09 20:51:40 +0530

    added exception.py

---
+++ src/exception.py
@@ -0,0 +1,34 @@
+import sys
+
+def error_message_details(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error = str(error)
+
+    error_message="Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name,
+        line_number,
+        error
+    )
+
+    # Parsed error message
+    return error_message
+
+class CustomException(Exception):
+    """
+        CustomException class to define  Custom Exceptions
+    """
+    def __init__(self, error_message, error_details: sys):
+        # Initiating the base class
+        super().__init__(error_message)
+
+        # Extracting the error message
+        self.error_message = error_message_details(
+            error_message, error_detail=error_details
+        )
+
+    def __str__(self):
+        return self.error_message
+
