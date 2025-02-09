cee391759dd981551977afbee52c56a1bc1adfe1
swan28 <swati28.g@gmail.com>
2025-02-09 20:54:23 +0530

    added constants

---
+++ src/constant/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+
+MONGO_DATABASE_NAME = "FaultDetectionProject"
+
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+
+MONGO_DB_URL = "mongodb+srv://swati:WC1QznMI2YPklMgD@cluster0.tybza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+MODEL_FILE_NAME = "model"
+
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder = "artifacts"
